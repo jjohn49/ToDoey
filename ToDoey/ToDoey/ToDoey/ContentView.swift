977211ac74bdb4b6fd08067a0845abd53fa98d02
jjohns49 <jjohns49@villanova.edu>
2022-04-24 19:44:19 -0400

    Need to delete the first project because it doesn't have a storybooard UI

---
+++ ToDoey/ToDoey/ToDoey/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ToDoey
+//
+//  Created by John Johnston on 4/21/22.
+//   Testing to see if this works
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ToDoey")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
