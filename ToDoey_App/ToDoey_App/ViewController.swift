d8d7f3f540dc30e301f2445d0ee5ed40edcc594b
jfacinelli <jfacinel@villanova.edu>
2022-04-25 14:29:15 -0400

    Synced tableView to ViewController.

--- ToDoey_App/ToDoey_App/ViewController.swift
+++ ToDoey_App/ToDoey_App/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let tableViewData = Array(repeating: "Item", count: 5)
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self,
+                               forCellReuseIdentifier: "TableViewCell")
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableViewData.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell",
+                                                     for: indexPath)
+            cell.textLabel?.text = self.tableViewData[indexPath.row]
+            return cell
+    }
 }
+
+
 
