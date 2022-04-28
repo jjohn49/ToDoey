7f273206935e174583361b4539e114cc53760dd0
jfacinelli <jfacinel@villanova.edu>
2022-04-28 12:51:29 -0400

    Attempting to get ReminderInfoViewController to work

--- ToDoey_App/ToDoey_App/ReminderInfoViewController.swift
+++ ToDoey_App/ToDoey_App/ReminderInfoViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ReminderInfoViewController: UIViewController {
 
+    @IBOutlet weak var reminderTitle: UILabel!
+    @IBOutlet weak var reminderDate: UILabel!
+    @IBOutlet weak var reminderInfo: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         }
 
-
+    @IBAction func info(segue:UIStoryboardSegue) {
+        let TableVC = segue.source as! TableViewController
+        //gets the reminder title
+        reminderTitle.text = TableVC.newReminder
+        //gets the details for the reminder
+        reminderInfo.text = TableVC.newReminderDetail
+        //gets the due date for the reminder
+        reminderDate.text = TableVC.newReminderDueDate
+        //creates a String array to act as the value in reminder info
+//        var detailsArray: [String] = []
+//        // adds the details to the value array
+//        detailsArray.append(newReminderDetail)
+//        //adds the due date to the value array
+//        detailsArray.append(newReminderDueDate)
+//
+//        reminderInfo.updateValue(detailsArray, forKey: newReminder)
+//
+//        //Checks if the reminder is empty
+//        //if it is empty it will add to data
+//        if newReminder != ""{
+//            data.append(newReminder)
+//            tableView.reloadData()
+//        }
+    }
         // Do any additional setup after loading the view.
     }
     
