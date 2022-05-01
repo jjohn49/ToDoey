47af91a251777235e3e0a146b9470dff785b8e0f
jjohns49 <jjohns49@villanova.edu>
2022-04-30 20:59:45 -0400

    GOT EVERYTHING TO WORK!!!!

--- ToDoey_App/ToDoey_App/TableViewController.swift
+++ ToDoey_App/ToDoey_App/TableViewController.swift
@@ -58,6 +58,7 @@ class TableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
+    //creates the plist in the documents folder
     func createPlist(){
         let filem = FileManager.default
         let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
@@ -105,9 +106,20 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete{
+            let del = data[indexPath.row]
+            //print(del)
             data.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        //add stuff here to delete stuff from plist
+            deleteFromPlist(key: del)
+        }
+    }
+    
+    func deleteFromPlist(key:String){
+        let path = getPath()
+        if FileManager.default.fileExists(atPath: path){
+            let data = NSMutableDictionary(contentsOfFile: path) ?? ["":[""]]
+            data.removeObject(forKey: key)
+            data.write(toFile: path, atomically: true)
         }
     }
     
